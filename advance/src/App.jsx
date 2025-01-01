b43f17d0c4bbe811ca19942d55c6aca93a40ca0f
zagvii <mirian.vieira.moreira@gmail.com>
2025-01-01 19:24:51 -0300

    props

--- advance/src/App.jsx
+++ advance/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
-import ConditionalRender from './components/ConditionalRender'
+import CarDetails from './components/CarDetails'
+//import ConditionalRender from './components/ConditionalRender'
+import ShowUserName from './components/ShowUserName'
 //import ListRender from './components/ListRender'
 //import ManageData from './components/ManageData'
 // import Gatinho from './assets/gatinho.jpg'
@@ -17,8 +19,15 @@ function App() {
         <img src={Gatinho} alt="Gatinho" />
       </div> 
       <ManageData />
-      <ListRender />*/}
-      <ConditionalRender />
+      <ListRender />
+      <ConditionalRender />*/}
+      {/* props */}
+      <ShowUserName name="Mirian" />
+      {/* destructuring props */}
+      <CarDetails brand="VW" km={10000} color="Branco" />
+      {/* reaproveitando */}
+      <CarDetails brand="Fiat" color="Azul" km={0} />
+      <CarDetails brand="Ford" color="Preto" km={4500} />
     </>
   )
 }
