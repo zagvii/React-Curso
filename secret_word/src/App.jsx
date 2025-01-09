1466d0180f45ed54e9d1e761539ca601651e1519
zagvii <mirian.vieira.moreira@gmail.com>
2025-01-09 20:59:57 -0300

    gameGuessWord-gamePage

--- secret_word/src/App.jsx
+++ secret_word/src/App.jsx
@@ -23,27 +23,69 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(3);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = () => {
+    // Picks random category
+    const categories = Object.keys(words);
+    // Math.random - Choose float number acordingly with the amount multiplied
+    // Math.floor - Transforms the float number to int
+    const category = categories[Math.floor(Math.random() * Object.keys(words).length)];
+
+    // Picks random word from category choosen
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    
+    return {category, word};
+  }
+
   // Starts game
   const startGame = () => {
-    setGameStage(stages[1].name)
+    // Choose word and category randomly
+    const {word, category} = pickWordAndCategory();
+
+    // Create letters array
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
+    console.log(word);
+
+    setGameStage(stages[1].name);
   }
 
   // Process letter input
   const verifyLetter = () => {
-    setGameStage(stages[2].name)
+    setGameStage(stages[2].name);
   }
 
   // Retry game
   const retry = () => {
-    setGameStage(stages[0].name)
+    setGameStage(stages[0].name);
   }
-
 
   return (
     <>
       <h1>Guess the word!</h1>
       {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} 
+                                      pickedWord={pickedWord}
+                                      pickedCategory={pickedCategory}
+                                      letters={letters}
+                                      guessedLetters={guessedLetters}
+                                      wrongLetters={wrongLetters}
+                                      guesses={guesses }
+                                      score={score}
+                                />}
       {gameStage === 'end' && <GameOver retry={retry}/>}
     </>
   )
--- secret_word/src/components/Game.jsx
+++ secret_word/src/components/Game.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react'
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
+
+  const [letter, setLetter] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
-      <button className="action-button" onClick={verifyLetter}>End Game</button>
+      <p className='points'>
+        <span>Points: {score}</span>
+      </p>
+      <p className='tip'>
+        <span>Word Tip: <span className='wordTip'>{pickedCategory}</span></span>
+      </p>
+      <p>You have {guesses} guess(es).</p>
+      <div className='wordContainer'>
+        {letters.map((l, i) => (
+          guessedLetters.includes(l) 
+          ? <span key={i} className='individualLetter'>A</span>
+          : <span key={i} className='blank'></span>
+        ))}
+      </div>
+      <div className='lettersContainer'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter}/>
+          <button className="action-button">Try</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Already Used Letters:</p>
+        {wrongLetters.map((l, i) => 
+          <span key={i}>{l}, </span>
+        )}
+      </div>
     </>
   )
 }
